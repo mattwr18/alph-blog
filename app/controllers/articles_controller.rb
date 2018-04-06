8871f87ba3de74d15ec7580f39896f37c088ee65
Matt Rider <mattwr18@gmail.com>
2018-04-06 18:03:20 -0300

    Add set article for show, edit, update, destroy

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,10 +1,11 @@
 class ArticlesController < ApplicationController
+  before_action :set_article, only: [:edit, :update, :show, :destroy] 
+
   def index
     @articles = Article.all
   end
 
   def show
-    @article = Article.find(params[:id])
   end
 
   def new
@@ -12,7 +13,6 @@ class ArticlesController < ApplicationController
   end
 
   def edit
-    @article = Article.find(params[:id])
   end
 
   def create
@@ -38,13 +38,16 @@ class ArticlesController < ApplicationController
     end
 
     def destroy
-      @article = Article.find(params[:id])
       @article.destroy
       flash[:notice] = "Article was successfully deleted"
       redirect_to articles_path
     end
 
   private
+
+  def set_article
+    @article = Article.find(params[:id])
+  end
 
   def article_params
     params.require(:article).permit(:title, :description)
