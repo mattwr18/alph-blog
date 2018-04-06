350b4d89e358d02432d214d3efc3358cc5f32add
Matt Rider <mattwr18@gmail.com>
2018-04-06 17:06:28 -0300

    Add edit, update, show and index, refactor edit, index views

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,16 +1,41 @@
 class ArticlesController < ApplicationController
+  def index
+    @articles = Article.all 
+  end
+
+  def show
+    @article = Article.find(params[:id])
+  end
+
   def new
     @article = Article.new
   end
 
+  def edit
+    @article = Article.find(params[:id])
+  end
+
   def create
     @article = Article.new(article_params)
+
     if @article.save
-      redirect_to articles_path
+      flash[:notice] = "Article was successfully created"
+      redirect_to article_path(@article)
     else
-      render 'new'  
+      render 'new'
     end
   end
+
+    def update
+      @article = Article.find(params[:id])
+
+      if @article.update(article_params)
+        flash[:notice] = "Article was successfully updated"
+        redirect_to article_path(@article)
+      else
+        render 'edit'
+      end
+    end
 
 
   private
