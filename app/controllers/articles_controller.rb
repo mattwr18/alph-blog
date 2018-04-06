e69b79a1a9c4c3fe223b7f307ebca654fe443858
Matt Rider <mattwr18@gmail.com>
2018-04-06 17:17:21 -0300

    Add delete functionality and link to blog on homepage

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,6 +1,6 @@
 class ArticlesController < ApplicationController
   def index
-    @articles = Article.all 
+    @articles = Article.all
   end
 
   def show
@@ -37,6 +37,12 @@ class ArticlesController < ApplicationController
       end
     end
 
+    def destroy
+      @article = Article.find(params[:id])
+      @article.destroy
+      flash[:notice] = "Article was successfully deleted"
+      redirect_to articles_path
+    end
 
   private
 
